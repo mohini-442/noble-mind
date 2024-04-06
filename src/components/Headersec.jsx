4a20c81758d7694eb4b0c55002f8906772126e5c
mohini-442 <mohinikarwasra598@gmail.com>
2024-04-06 10:58:51 -1200

    make an icons component

--- src/components/Headersec.jsx
+++ src/components/Headersec.jsx
@@ -1,3 +1,4 @@
+import { Starsvg } from "./Icons";
 
 export default function Headersec() {
     return (
@@ -7,23 +8,7 @@ export default function Headersec() {
                     <div className="flex flex-wrap flex-row">
                         <div className="lg:w-7/12 flex justify-center flex-col">
                             <div className="flex items-center gap-1 mx-auto lg:mx-0" data-aos="zoom-y-out" data-aos-delay="2500">
-                                <span><svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M13.9187 6.87573L11.1603 14.2314L3.80469 16.9897L11.1603 19.7481L13.9187 27.1037L16.677 19.7481L24.0327 16.9897L16.677 14.2314L13.9187 6.87573Z" fill="#00EAEC" />
-                                    <path d="M13.9187 6.87573L11.1603 14.2314L3.80469 16.9897L11.1603 19.7481L13.9187 27.1037L16.677 19.7481L24.0327 16.9897L16.677 14.2314L13.9187 6.87573Z" fill="url(#paint0_linear_101_304)" />
-                                    <path d="M5.60149 1.11942L4.23608 5.25408L0.223112 6.94403L4.35777 8.30944L6.04772 12.3224L7.41314 8.18774L11.4261 6.4978L7.29144 5.13238L5.60149 1.11942Z" fill="#00EAEC" />
-                                    <path d="M5.60149 1.11942L4.23608 5.25408L0.223112 6.94403L4.35777 8.30944L6.04772 12.3224L7.41314 8.18774L11.4261 6.4978L7.29144 5.13238L5.60149 1.11942Z" fill="url(#paint1_linear_101_304)" />
-                                    <defs>
-                                        <linearGradient id="paint0_linear_101_304" x1="6.11235" y1="0.224727" x2="69.1736" y2="54.8026" gradientUnits="userSpaceOnUse">
-                                            <stop stop-color="#A854E9" />
-                                            <stop offset="1" stop-color="#4F91FC" />
-                                        </linearGradient>
-                                        <linearGradient id="paint1_linear_101_304" x1="1.13135" y1="-2.39195" x2="37.2609" y2="26.4442" gradientUnits="userSpaceOnUse">
-                                            <stop stop-color="#A854E9" />
-                                            <stop offset="1" stop-color="#4F91FC" />
-                                        </linearGradient>
-                                    </defs>
-                                </svg>
-                                </span>
+                                <Starsvg />
                                 <p className="font-normal text-[16px] font-Exo 2 text-[#131200]" >AI With Noble Mind</p>
                             </div>
                             <h2 className=" font-Outfit font-light text-[34px] md:text-[64px] leading-[38px] md:leading-[66px] text-black lg:pt-3 text-center lg:text-start " data-aos="zoom-y-out" data-aos-delay="2500">Inspiring Innovation, Elevating Solutions – <span className=" font-Outfit text-[30px] md:text-[48px] font-semibold leading-[36px] lg:leading-[57px]"> Noble Mind &#34;  </span></h2>
--- src/components/Whychooseus.jsx
+++ src/components/Whychooseus.jsx
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import chooseus from '../../public/assets/images/whychooseus.png';
 import Image from "next/image";
 import ellipse from '../../public/assets/images/ellipse2.png'
+import { Starsvg } from "./Icons";
 
 export default function Whychooseus() {
     const teamcard = [
@@ -82,23 +83,7 @@ export default function Whychooseus() {
             <div className="relative z-[2] overflow-hidden">
                 <div className="max-w-[1140px] xxl:max-w-[1440px] px-3 mx-auto pt-2 lg:py-12" id="whychooseus">
                     <div className="flex items-center justify-center gap-1 pt-3" data-aos="fade-left">
-                        <span><svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M13.9187 6.87573L11.1603 14.2314L3.80469 16.9897L11.1603 19.7481L13.9187 27.1037L16.677 19.7481L24.0327 16.9897L16.677 14.2314L13.9187 6.87573Z" fill="#00EAEC" />
-                            <path d="M13.9187 6.87573L11.1603 14.2314L3.80469 16.9897L11.1603 19.7481L13.9187 27.1037L16.677 19.7481L24.0327 16.9897L16.677 14.2314L13.9187 6.87573Z" fill="url(#paint0_linear_101_304)" />
-                            <path d="M5.60149 1.11942L4.23608 5.25408L0.223112 6.94403L4.35777 8.30944L6.04772 12.3224L7.41314 8.18774L11.4261 6.4978L7.29144 5.13238L5.60149 1.11942Z" fill="#00EAEC" />
-                            <path d="M5.60149 1.11942L4.23608 5.25408L0.223112 6.94403L4.35777 8.30944L6.04772 12.3224L7.41314 8.18774L11.4261 6.4978L7.29144 5.13238L5.60149 1.11942Z" fill="url(#paint1_linear_101_304)" />
-                            <defs>
-                                <linearGradient id="paint0_linear_101_304" x1="6.11235" y1="0.224727" x2="69.1736" y2="54.8026" gradientUnits="userSpaceOnUse">
-                                    <stop stop-color="#A854E9" />
-                                    <stop offset="1" stop-color="#4F91FC" />
-                                </linearGradient>
-                                <linearGradient id="paint1_linear_101_304" x1="1.13135" y1="-2.39195" x2="37.2609" y2="26.4442" gradientUnits="userSpaceOnUse">
-                                    <stop stop-color="#A854E9" />
-                                    <stop offset="1" stop-color="#4F91FC" />
-                                </linearGradient>
-                            </defs>
-                        </svg>
-                        </span>
+                       <Starsvg/>
                         <p className="font-normal text-[16px] font-Exo 2 text-[#131200]" data-aos="fade-left">Why Choose Us</p>
                     </div>
                     <h2 className=" font-Outfit font-light max-w-[552px] mx-auto text-[32px] md:text-[48px] leading-[36px] md:leading-[58px] pt-2 text-center" data-aos="fade-left"> Driving Innovation &  <span className="font-semibold">Transforming Industries</span></h2>
