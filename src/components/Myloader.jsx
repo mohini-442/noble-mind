61ce3826c4116957977c1a29e736f183b7f04789
mohini-442 <mohinikarwasra598@gmail.com>
2024-04-06 23:07:18 -1200

    updates erroe solve

--- src/components/Myloader.jsx
+++ src/components/Myloader.jsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 
 export default function Myloader() {
     const [loder, setloader] = useState(true);
-    const bodyRef = useRef(document.body);
 
     useEffect(() => {
         setTimeout(() => {
